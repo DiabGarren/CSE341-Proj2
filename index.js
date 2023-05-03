9bf90c7b9b1883c0d79134d44bee8089c5760316
DiabGarren <garrendiab@gmail.com>
2023-05-03 22:45:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app
+    // .use(cors())
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        next();
+    })
+    .use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
+});
